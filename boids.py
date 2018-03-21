2ffcca4160cf53993a8fd292c41ad11d5efc9707
Spencer Edwards <13srte@caslab.queensu.ca>
2018-03-21 14:21:55 -0400

    CR boids system program start

---
+++ boids.py
@@ -0,0 +1,15 @@
+# Assignment 3 - Artificial Life - CISC 352
+# Spencer Edwards - 13srte
+# Hannah LeBlanc - 12hml4
+
+#
+# “I confirm that this submission is my own work and is consistent with
+# the Queen's regulations on Academic Integrity.”
+#
+
+def runBoids():
+
+def main():
+    runBoids()
+
+main()
