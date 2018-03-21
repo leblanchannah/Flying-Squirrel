2435271292b43feab2d81df35b99ae777672b3af
Spencer Edwards <13srte@caslab.queensu.ca>
2018-03-21 14:49:55 -0400

    CR boids system program start

--- boids.py
+++ boids.py
@@ -7,9 +7,61 @@
 # the Queen's regulations on Academic Integrity.”
 #
 
-def runBoids():
+import time
+
+class Boids:
+
+    def __init__(self, n, interval):
+        self.n = n     # number of boids
+        self.interval = interval
+        self.boids = [[0, 0]] * n      # boids[i] = (x, y)   ie. position of boid i
+        self.velocities = [[0, 0]] * n # positions[i] = (vx, vy) ie. velocity in each direction of boid i
+        runBoids(interval)
+
+    def runBoids(self, interval):
+        while True:
+            start = time.time()
+
+            # drawBoids()
+            moveAllBoids()
+
+            end = time.time()
+            timeElapsed = end - start
+            time.sleep(interval - timeElapsed)
+
+    def moveAllBoids(self):
+        v1 = None
+        v2 = None
+        v3 = None
+        b = None
+        for i in range(len(self.boids)):
+            b = self.boids[i]
+            v1 = self.separation(b)
+            v2 = self.alignment(b)
+            v3 = self.cohesion(b)
+            sum = v1 + v2 + v3
+
+            self.velocities[i] = self.velocities[i] + sum
+            self.boids[i] = self.boids + self.velocities[i]
+
+    def separation(self,b):
+        # v = None
+        v = b
+        return b
+
+    def alignment(self, b):
+        # v = None
+        v = b
+        return b
+
+    def cohesion(self, b):
+        # v = None
+        v = b
+        return b
 
 def main():
-    runBoids()
+    n = 100         # number of boids
+    interval = 1    # move boids every interval (in seconds)
+    b = Boids(n, interval)
 
 main()
