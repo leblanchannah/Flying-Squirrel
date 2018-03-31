e2b06b9aa86840131ed116dcea1a0b0d2a6fca3a
Hannah LeBlanc <leblanchannah95@gmail.com>
2018-03-31 19:12:10 -0400

    The Tkinter animation is working but code needs to polished

--- life.py
+++ life.py
@@ -1,25 +1,20 @@
 import copy
 from tkinter import *
+import time
 
 graph = []
+m = 25
 
-class Window(Frame):
-    def __init__(self,master = None):
-        Frame.__init__(self, master)
-        self.master = master 
-    
-    def init_window(self):
-        self.master.title("Conway's Life")
-        self.pack(fill=BOTH, expand=1)
-
-class Conway:
-    def __init__(self,generations,graph,root):
+class Conway(Frame):
+    def __init__(self,generations,game):
         self.gen = generations
-        self.game = graph
-        self.rows = len(graph) -1
-        self.cols = len(graph[0]) -1 
+        self.game = game
+        self.rows = len(self.game) -1
+        self.cols = len(self.game[0]) -1 
         self.m = 25
         self.graph = []
+        self.build_graph()
+        
         # game runs gen times, 1 = alive, 0 = dead
         # Calculate N, the N of live cells in C's eight-location neighborhood. 
         # need to change cell from (0,0).
@@ -32,15 +27,55 @@ class Conway:
                     self.n = self.nearby(self.tempG,cell)
                     self.game[cell[0]][cell[1]] = self.deadOrAlive(self.n,cell) #determine if cell is dead or alive
             self.draw()
+            time.sleep(1)
             self.printGen(i+1)
+        mainloop()
             
-        
+    def update(self):
+        self.draw()    
+        graph.after(500,update)
+
+    def init_window(self):
+        self.master.title("Conway's Life")
+        self.pack(fill=BOTH, expand=1)
+
     def printGen(self,gen):
         print("Generation:" +str(gen))
         for i in range(self.rows):
             print("".join(str(self.game[i])))
 
+    def draw(self):
+        newGrid = self.game
+        row = 0   
+        global graph  
+        while row < len(newGrid):        
+            col = 0        
+            while col < len(newGrid[0]):            
+                cell = newGrid[row][col]            
+                startX = self.m*col            
+                endX = startX+self.m           
+                startY = self.m*row            
+                endY = startY+self.m            
+                if cell == 1:                
+                    graph.create_rectangle(startX,startY,endX,endY,fill="red")            
+                else:                
+                    graph.create_rectangle(startX,startY,endX,endY,fill="black")            
+                col = col+1         
+            row = row+1   
+            graph.update()
 
+    def build_graph(self):    
+        global graph    
+        global m
+        print(graph)
+        WIDTH = m*len(self.game[0])
+        HEIGHT = m*len(self.game)    
+        root = Tk()    
+        root.overrideredirect(True)    
+        root.geometry('%dx%d+%d+%d' % (WIDTH, HEIGHT, (root.winfo_screenwidth() - WIDTH) / 2, (root.winfo_screenheight() - HEIGHT) / 2))    
+        root.bind_all('<Escape>', lambda event: event.widget.quit())    
+        graph = Canvas(root, width=WIDTH, height=HEIGHT, background='white')      
+        graph.pack()
 
     def deadOrAlive(self, N, cell):
         if self.tempG[cell[0]][cell[1]] == 0: # cell of interest is dead
@@ -90,18 +125,10 @@ def IOGame(inFile, outFile):
     for line in f:
         temp = list(line)[:-1]
         graph.append([int(x) for x in temp]) #removing \n char from line
-
+    game = copy.deepcopy(graph)
     # need to print each thing to the file, open file and pass "out" to object??
-    root = Tk()
-    m = 25
-    WIDTH = m*len(graph[0])
-    HEIGHT = m*len(graph)
-    root.geometry('%dx%d+%d+%d' % (WIDTH, HEIGHT, (root.winfo_screenwidth() - WIDTH) / 2, (root.winfo_screenheight() - HEIGHT) / 2))
-    app = Window(root)
-    root.bind_all('<Escape>', lambda event: event.widget.quit())
-    graph = Canvas(root, width=WIDTH, height=HEIGHT, background='white')
-    root.mainloop()
-    Conway(generations, graph, root)
+
+    Conway(generations, game)
 
 
 def main():
