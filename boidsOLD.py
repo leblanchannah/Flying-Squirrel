7550f4e75689d02d96c36150a7301a742fe0743d
Spencer Edwards <13srte@caslab.queensu.ca>
2018-04-02 19:38:02 -0400

    final version there

--- boidsOLD.py
+++ boidsOLD.py
@@ -22,14 +22,14 @@ class Boids:
         self.height = 600
         self.centreX = self.width // 2
         self.centreY = self.height // 2
+        print(str(self.centreX) + ", " + str(self.centreY))
         self.velocities = [[0, 0] for i in range(n)] # positions[i] = (vx, vy) ie. velocity in each direction of boid i
         self.wall = 10
-        self.wallForce = 5
+        self.wallForce = 10
         self.boidSize = 6
         self.startTime = 0
         self.timeElapsed = 0
-        self.offsetStart = 20
-        
+
         root = Tk()
         root.title("Hello")
         root.overrideredirect(True)
@@ -37,37 +37,11 @@ class Boids:
                                        (root.winfo_screenheight() - self.height) / 2))
         root.bind_all('<Escape>', lambda event: event.widget.quit())
         self.wn = Canvas(root, width=self.width, height=self.height, background='white')
-        #self.random_start()
         self.initializeBoids()
         self.wn.after(interval, self.runBoids)
         self.wn.pack()
         mainloop()
 
-
-    def random_start(self):
-        for i in range(self.n):
-            if random.randint(0,1):
-                #along left and right
-                y = random.randint(1,self.height)
-                if random.randint(0,1):
-                    #along left
-                    x = -self.offsetStart
-                else:
-                    x = self.width + self.offsetStart
-            else:
-                #along top and bottom
-                x = random.randint(1,self.width)
-                if random.randint(0,1):
-                    #along top
-                    y = -self.offsetStart
-                else:
-                    y = self.height + self.offsetStart
-            self.boids[i][0] = x
-            self.boids[i][1] = y
-            self.velocities[i] = [0,0]
-            self.startTime = time.time()
-
-        
     def initializeBoids(self):
         quarter = self.n / 4
         #Place boids on screen with equal distribution
@@ -115,9 +89,9 @@ class Boids:
         centreY = boid[1]
 
         leftX = centreX - self.boidSize
-        leftY = centreY - (self.boidSize)
+        leftY = centreY - (self.boidSize / 2)
         rightX = centreX + self.boidSize
-        rightY = centreY + (self.boidSize)
+        rightY = centreY + (self.boidSize / 2)
 
         self.wn.create_oval(leftX, leftY, rightX, rightY)
         self.wn.update()
@@ -134,23 +108,14 @@ class Boids:
         for i in range(len(self.boids)):
             b = self.boids[i]
             v1 = self.cohesion(b, i)
-            #print("x v1 = " + str(v1[0]) + " y v1 = " + str(v1[1]))
             v2 = self.separation(b, i)
-            #print("x v2 = " + str(v2[0]) + " y v2 = " + str(v2[1]))
             v3 = self.alignment(b, i)
-            #print("x v3 = " + str(v3[0]) + " y v3 = " + str(v3[1]))
             sum = vectorAdd(v1, vectorAdd(v2, v3))
-            #print("sum x v1+v2+v3 = " + str(sum[0]) + " sum y v1+v2+v3 = " +str(sum[1]))
             if (self.timeElapsed > 15) and (self.timeElapsed < 25):
                 v4 = self.wind()
                 sum = vectorAdd(sum, v4)
 
             self.velocities[i] = vectorAdd(self.velocities[i], sum)
-            print(self.velocities[i])
-            
-        for i in range(len(self.boids)):
-            self.velocities[i][0] /= 5
-            self.velocities[i][1] /= 5
             self.boids[i] = vectorAdd(self.boids[i], self.velocities[i])
 
     def cohesion(self, bj, ix):
@@ -160,15 +125,16 @@ class Boids:
             if b != bj:
                 pcj = vectorAdd(pcj, self.boids[i])
         pcj = [pcj[0] / (self.n - 1), pcj[1] / (self.n - 1)]
-        #print("x pcj = " + str(pcj[0]) + " y pcj = " + str(pcj[1]))
-        return [(pcj[0] - self.boids[ix][0]) / 8, (pcj[1] - self.boids[ix][1]) / 8]
+
+        print("pcj :[" + str(pcj[0]) + ", " + str(pcj[1]) + "]")
+        return [(pcj[0] - self.boids[ix][0]) / 100, (pcj[1] - self.boids[ix][1]) / 100]
 
     def separation(self, bj, ix):
         c = [0, 0]
         for i in range(len(self.boids)):
             b = self.boids[i]
             if b != bj:
-                if euclideanDistance(self.boids[i], self.boids[ix]) < 35:
+                if euclideanDistance(self.boids[i], self.boids[ix]) < 15:
                     c = vectorSubtract(c, vectorSubtract(self.boids[i],  self.boids[ix]))
         return c
 
@@ -179,11 +145,10 @@ class Boids:
             if b != bj:
                 pvj = vectorAdd(pvj, self.velocities[i])
         pvj = [pvj[0] / (self.n - 1), pvj[1] / (self.n - 1)]
-        #print("x pvj = " + str(pvj[0]) + " y pvj = " + str(pvj[1]))
-        return [(pvj[0] - self.velocities[ix][0]) / 16, (pvj[1] - self.velocities[ix][1]) / 16]
+        return [(pvj[0] - self.velocities[ix][0]) / 8, (pvj[1] - self.velocities[ix][1]) / 8]
 
     def wind(self):
-        v = [0, 25]
+        v = [0, 5]
         return v
 
     def checkWalls(self, boid, ix):
@@ -195,7 +160,7 @@ class Boids:
             self.velocities[ix][0] -= self.wallForce
         if y < self.wall:
             self.velocities[ix][1] += self.wallForce
-        elif y > (self.height - self.wall):
+        elif y < (self.height - self.wall):
             self.velocities[ix][1] -= self.wallForce
 
     def angleBetween(self, v):
